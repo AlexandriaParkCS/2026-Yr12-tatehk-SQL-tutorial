8c2574a6814a5aa3dac794538e773f24b68803dd
Tate HeggatonKlenner <tate.heggatonklenner@education.nsw.gov.au>
2026-01-28 14:16:48 +1100

    Completed practical 4:  Aggregate functions and group by
    Done!

--- src/sqlite_practical_004.sql
+++ src/sqlite_practical_004.sql
@@ -23,9 +23,85 @@ SELECT
     avg(height) as average_height,
     max(height) as tallest,
     min(height) as shortest
-from characters
+from characters;
 
-select species count(*) as character_count
+SELECT column, AGGREGATE_FUNCTION(column)
+FROM table_name
+GROUP BY column;
+
+select species, count(*) as character_count
 from characters
 group by species;
 
+SELECT species, AVG(height) AS average_height
+from characters
+where height is not null
+group by species;
+
+select homeworld, count(*) as character_count
+from characters 
+group by homeworld
+order by character_count desc;
+
+alter table characters add column affiliation text;
+
+update characters set affiliation = 'Rebel Alliance' where name in ('luke skywalker', 'Leia Organa', 'Han Solo', 'Chewbacca');
+
+update characters set affiliation = 'Jedi Order' where name in ('Obi-Wan Kenobi', 'Yoda');
+
+update characters set affiliation = 'Galactic Empire' where name = 'Darth Vader';
+
+update characters set affiliation = 'Independent' where name = 'R2-D2';
+
+
+select affiliation, count(*) as members
+from characters
+where affiliation is not null
+group by affiliation
+order by members desc;
+
+select column, aggregate_function(COLUMN)
+from table_name
+group by COLUMN
+having condition;
+
+select species, count(*) as character_count
+from characters
+group by species
+having count(*) >= 2;
+
+select affiliation, count(*) as member_count
+from characters
+where affiliation is not null
+group by affiliation
+having count(*) > (select avg(cnt) from (select count(*) as cnt from characters where affiliation is not null group by affiliation));
+
+select homeworld, count(*) as human_count
+from characters
+where species = 'Human'
+group by homeworld 
+having count(*) >= 2;
+
+select count(distinct species) as unique_species
+from characters;
+
+select count(distinct homeworld) as unique_homeworlds
+from characters;
+
+select sum(height) as total_height
+from characters;
+
+select homeworld, count(*) as character_count
+from characters
+group by homeworld
+order by homeworld;
+
+select affiliation, avg(height) as avg_height
+from characters
+where height is not null and affiliation is not null
+group by affiliation;
+
+select homeworld, count(*) as character_count
+from characters 
+group by homeworld
+having count(*) = 1;
